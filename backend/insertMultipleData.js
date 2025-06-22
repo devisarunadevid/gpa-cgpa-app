06a4ee009f103934104496c93edb3269345d5704
Devis Aruna Devi D <23cseb04.devisarunadevi.d@gmail.com>
2025-06-22 13:37:06 +0530

    updated

--- backend/insertMultipleData.js
+++ backend/insertMultipleData.js
@@ -5,7 +5,10 @@ const Student = require("./models/Student");
 dotenv.config();
 
 mongoose
-  .connect(process.env.MONGO_URI)
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => console.log("MongoDB Connected"))
   .catch((err) => console.log(err));
 
--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,10 @@ app.use(cors(corsOptions));
 dotenv.config();
 
 mongoose
-  .connect(process.env.MONGO_URI)
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => console.log("MongoDB Connected"))
   .catch((err) => console.log(err));
 
