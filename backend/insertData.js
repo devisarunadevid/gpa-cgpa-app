f56ec27f7fd499c81dd14de561affa6b66f46647
Devis Aruna Devi D <23cseb04.devisarunadevi.d@gmail.com>
2025-06-22 13:19:11 +0530

    Connected to gpa-cgpa-db and fixed Student model

--- backend/insertData.js
+++ backend/insertData.js
@@ -4,10 +4,7 @@ const Student = require("./models/Student");
 dotenv.config();
 
 mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB Connected"))
   .catch((err) => console.log(err));
 
--- backend/server.js
+++ backend/server.js
@@ -18,10 +18,7 @@ app.use(cors(corsOptions));
 dotenv.config();
 
 mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB Connected"))
   .catch((err) => console.log(err));
 
